86e45792c8c108ed1353a38bbc94c04e1707814a
DustUnderBag <usamary55@gmail.com>
2025-02-19 16:08:33 -0500

    Set up eslint with basic configurations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./reset.css";
 
 console.log("Script entry point working");
+
+class People {
+    constructor(name, gender, age, country) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.country = country;
+    }
+
+    eat() {
+        
+    }
+
+    get intro() {
+        return `${this.name} is from ${this.country}!`;
+    }
+}
+
+const ppl = new People("John", "Male", 23, "Canada");
+console.log(ppl.age);
+console.log(ppl.intro);
