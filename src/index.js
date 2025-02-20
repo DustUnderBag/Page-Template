8ae18df84593be8b74fe3de554afdcff41d6a70b
DustUnderBag <usamary55@gmail.com>
2025-02-20 04:05:59 -0500

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import "./reset.css";
 
 console.log("Script entry point working");
-
-class People {
-  constructor(name, gender, age, country) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.country = country;
-  }
-
-  eat() {}
-
-  get intro() {
-    return `${this.name} is from ${this.country}!`;
-  }
-}
-
-const ppl = new People("John", "Male", 23, "Canada");
