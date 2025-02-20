50667a9231b555453ed1656e71311bcd2eeccc69
DustUnderBag <usamary55@gmail.com>
2025-02-20 04:03:37 -0500

    Set up prettier, git-hook
    - Set up prettier in local project,
    - Set up eslint-config-prettier, to avoid conflict between the two,
    - Set up formatting with prettier before committing,

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import "./reset.css";
 console.log("Script entry point working");
 
 class People {
-    constructor(name, gender, age, country) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.country = country;
-    }
+  constructor(name, gender, age, country) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.country = country;
+  }
 
-    eat() {
-        
-    }
+  eat() {}
 
-    get intro() {
-        return `${this.name} is from ${this.country}!`;
-    }
+  get intro() {
+    return `${this.name} is from ${this.country}!`;
+  }
 }
 
 const ppl = new People("John", "Male", 23, "Canada");
-console.log(ppl.age);
-console.log(ppl.intro);
